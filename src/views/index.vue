e23af2b5e8df9a586e4fea16a174681a431a6b76
likai <1195419506@qq.com>
2020-03-05 10:25:06 +0800

    使用elementUI的侧边栏

--- src/views/index.vue
+++ src/views/index.vue
@@ -8,6 +8,10 @@
             alt=""
           />
         </div>
+        <div class="contracted-state-panel" @click="zoom">
+          <i class="el-icon-s-fold" v-if="isCollapse === false" />
+          <i class="el-icon-s-unfold" v-else />
+        </div>
         <div class="greetings-panel">
           <p>你好! 用户名</p>
         </div>
@@ -23,32 +27,52 @@
     </div>
     <div class="content-panel">
       <div class="main-panel">
-        <div class="side-panel">
-          <div class="menu-panel">
-            <div
-              class="row-panel"
-              @click="switchMenu(index)"
-              v-for="(item, index) in menus"
-              v-bind:key="item.id"
-              v-bind:class="{ active: index === current }"
-            >
+        <div
+          class="side-panel"
+          :style="{ width: isCollapse === false ? '10%' : 'auto' }"
+        >
+          <el-menu
+            default-active="1-1"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            :collapse="isCollapse"
+          >
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location" />
+                <span slot="title">导航一</span>
+              </template>
+              <el-menu-item-group>
+                <span slot="title">分组一</span>
+                <el-menu-item index="1-1" @click="showContent('选项一')">
+                  选项1
+                </el-menu-item>
+                <el-menu-item index="1-2" @click="showContent('选项二')">
+                  选项2
+                </el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3" @click="showContent('选项三')">
+                  选项3
+                </el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <span slot="title">选项4</span>
+                <el-menu-item index="1-4-1" @click="showContent('选项一')">
+                  选项1
+                </el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2" @click="showContent('导航二')">
               <i class="el-icon-menu" />
-              <p>
-                {{ item.title }}
-              </p>
-            </div>
-          </div>
-          <div class="more-panel">
-            <div
-              class="row-panel"
-              v-for="(item, index) in menuList"
-              v-bind:key="item.id"
-              v-bind:class="{ active: index === currentMore }"
-              @click="showContent(item.title, index)"
-            >
-              <p>{{ item.title }}</p>
-            </div>
-          </div>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" @click="showContent('导航三')">
+              <i class="el-icon-setting" />
+              <span slot="title">导航三</span>
+            </el-menu-item>
+          </el-menu>
         </div>
         <div class="main-content">
           <router-view name="contentBody" />
@@ -66,40 +90,13 @@ export default {
       current: 0,
       currentMore: 0,
       tabPosition: "left",
-      menus: [
-        {
-          title: "菜单1",
-          id: "1"
-        },
-        {
-          title: "菜单2",
-          id: "2"
-        },
-        {
-          title: "菜单3",
-          id: "3"
-        }
-      ],
-      menuList: [
-        {
-          title: "选项1",
-          id: "12901"
-        },
-        {
-          title: "选项2",
-          id: "12902"
-        },
-        {
-          title: "选项3",
-          id: "12903"
-        }
-      ]
+      // 控制侧栏缩放状态
+      isCollapse: false
     };
   },
   mounted() {
-    if (this.$route.params.menuName === "导航1") {
-      this.currentMore = 0;
-      this.showContent("导航1");
+    if (this.$route.params.menuName !== "选项1") {
+      this.showContent("选项1");
     }
   },
   methods: {
@@ -110,9 +107,14 @@ export default {
         this.$router.push({ name: "contentBody", params: { menuName: val } });
       }
     },
-    switchMenu(index) {
-      this.current = index;
-      console.log(index);
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    zoom() {
+      this.isCollapse = !this.isCollapse;
     }
   }
 };
