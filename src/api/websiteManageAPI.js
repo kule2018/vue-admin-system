be5a6d7d52146278160b3e063226bb8b7412bb03
MagicalProgrammer <645289137@qq.com>
2020-03-10 18:13:49 +0800

    修复接口调用方式

--- src/api/websiteManageAPI.js
+++ src/api/websiteManageAPI.js
@@ -6,7 +6,13 @@ import base from "./base"; // 导入接口域名列表
 const websiteManageAPI = {
   // 登录
   login(params) {
-    return services._axios.post(`${base.defaultBaseUrl}/login`, params);
+    return services._axios.post(`${base.defaultBaseUrl}/sys/login`, params);
+  },
+  testLogin() {
+    return services._axios.post(
+      `${base.defaultBaseUrl}/sys/login?name=hdtest&pwd=123456`,
+      {}
+    );
   },
   // 注册
   registered(params) {
