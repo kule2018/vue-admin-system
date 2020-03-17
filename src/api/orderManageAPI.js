4403d2aaff2195740aa6766d63b208ef92f9b552
erakon <13431445070@sina.cn>
2020-03-17 18:00:30 +0800

    对接订单管理接口,优化详情页全局样式

--- src/api/orderManageAPI.js
+++ src/api/orderManageAPI.js
@@ -21,6 +21,47 @@ const orderManageAPI = {
       `${base.defaultBaseUrl}/sys/material/add`,
       params
     );
+  },
+  // 变动订单
+  updateOrderInfo(params) {
+    return services._axios.post(
+      `${base.defaultBaseUrl}/sys/material/update`,
+      params
+    );
+  },
+  // 商品数据状态
+  getMaterialStatus(params) {
+    return services._axios.get(
+      `${base.defaultBaseUrl}/common/materialStatus/list`,
+      { params }
+    );
+  },
+  // 商品单位
+  getMaterialUnit(params) {
+    return services._axios.get(
+      `${base.defaultBaseUrl}/common/materialUnit/list`,
+      { params }
+    );
+  },
+  // 商品品牌
+  getMaterialBrand(params) {
+    return services._axios.get(
+      `${base.defaultBaseUrl}/common/materialBrand/list`,
+      { params }
+    );
+  },
+  // 商品大分类
+  getClassify(params) {
+    return services._axios.get(`${base.defaultBaseUrl}/common/classify/list`, {
+      params
+    });
+  },
+  // 商品小分类
+  getMaterialCategory(params) {
+    return services._axios.get(
+      `${base.defaultBaseUrl}/common/materialCategory/list`,
+      { params }
+    );
   }
 };
 
