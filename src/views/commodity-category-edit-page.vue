2c02a544523332f2ad1541c195a6a4e9b94e9f6e
MagicalProgrammer <645289137@qq.com>
2020-03-19 18:12:51 +0800

    feat(商品类别列表): 对接商品类别列表接口
    添加商品类别编辑页面代码

---
+++ src/views/commodity-category-edit-page.vue
@@ -0,0 +1,214 @@
+<!--商品类别编辑-->
+<template>
+  <div id="layerContent" class="sys-user-edit">
+    <div class="content-panel">
+      <div class="main-content">
+        <el-form ref="form" :model="form" :rules="rules" label-width="130px">
+          <el-form-item label="类别名" prop="name">
+            <el-input
+              v-model="form.name"
+              size="small"
+              placeholder="请输入类别名"
+            />
+          </el-form-item>
+          <el-form-item label="商品分类id" prop="classifyId">
+            <el-input
+              v-model="form.classifyId"
+              size="small"
+              placeholder="请输入分类id"
+            />
+          </el-form-item>
+          <el-row>
+            <el-col :span="24">
+              <el-form-item label="类别图" prop="icon">
+                <el-image
+                  :class="{ hide: !form.icon }"
+                  :src="iconPathAndBase"
+                  :preview-src-list="[iconPathAndBase]"
+                  class="avatar"
+                >
+                </el-image>
+                <el-upload
+                  class="avatar-uploader"
+                  :class="{ hide: form.icon }"
+                  action="http://172.16.0.110/common/upload"
+                  :show-file-list="false"
+                  :on-success="uploadSuccess"
+                  :before-upload="beforeUpload"
+                >
+                  <i
+                    class="el-icon-plus avatar-uploader-icon"
+                    ref="uploadFile"
+                  />
+                </el-upload>
+                <el-button size="mini" plain @click="$refs.uploadFile.click()"
+                  >上传</el-button
+                >
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form>
+      </div>
+    </div>
+    <div class="btn-panel">
+      <div class="main-content">
+        <el-button @click="cancel" size="small">取消</el-button>
+        <el-button
+          type="primary"
+          @click="onSubmit"
+          size="small"
+          :loading="submissionStatus"
+          >确定</el-button
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import lodash from "lodash";
+import base from "@/api/base";
+
+export default {
+  name: "commodity-category-edit-page",
+  data() {
+    return {
+      form: {
+        // 类别名
+        name: "",
+        // 类别图
+        icon: "",
+        classifyId: "",
+        // 排序号
+        sortNumber: ""
+      },
+      rules: {
+        name: [{ required: true, message: "请输入类别名", trigger: "blur" }],
+        classifyId: [
+          { required: true, message: "请输入分类id", trigger: "blur" }
+        ],
+        icon: [{ required: true, message: "请上传类别图" }]
+      },
+      // 提交状态
+      submissionStatus: false
+    };
+  },
+  methods: {
+    onSubmit() {
+      // 表单验证
+      this.$refs.form.validate(valid => {
+        this.submissionStatus = true;
+        if (valid) {
+          if (this.parentData.state === "add") {
+            this.$api.commodityCategoryMangeAPI
+              .addCommodityCategory(this.form)
+              .then(res => {
+                this.submissionStatus = false;
+                if (lodash.isEqual(res.code, "success")) {
+                  this.submissionStatus = false;
+                  this.$vb.plugin.showMsg("新增成功");
+                  // 刷新父页面
+                  this.$parent.search();
+                  // 关闭弹层
+                  this.$layer.close(this.layerid);
+                } else {
+                  // 关闭弹层
+                  this.$layer.close(this.layerid);
+                  this.$vb.plugin.message.error("商品分类新增失败");
+                }
+              });
+          } else {
+            this.$api.commodityCategoryMangeAPI
+              .changeCommodityCategory(this.form)
+              .then(res => {
+                if (lodash.isEqual(res.code, "success")) {
+                  this.submissionStatus = false;
+                  this.$vb.plugin.showMsg("变动成功");
+                  // 刷新父页面
+                  this.$parent.search();
+                  // 关闭弹层
+                  this.$layer.close(this.layerid);
+                } else {
+                  // 关闭弹层
+                  this.$layer.close(this.layerid);
+                  this.$vb.plugin.message.error("商品分类变动失败");
+                }
+              });
+            this.submissionStatus = false;
+          }
+        } else {
+          // 验证不通过
+          this.submissionStatus = false;
+          return false;
+        }
+      });
+    },
+    cancel() {
+      this.$layer.close(this.layerid);
+    },
+    // eslint-disable-next-line no-unused-vars
+    uploadSuccess(res) {
+      this.form.icon = res.data.path;
+    },
+    beforeUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    }
+  },
+  computed: {
+    iconPathAndBase() {
+      return base.defaultBaseUrl + this.form.icon;
+    }
+  },
+  mounted() {
+    if (this.parentData.state === "update") {
+      this.$api.commodityCategoryMangeAPI
+        .getCommodityCategoryList({
+          classifyId: this.parentData.classifyId
+        })
+        .then(res => {
+          if (lodash.isEqual(res.code, "success")) {
+            // console.log(res.data);
+            this.form = res.data[0];
+          } else {
+            this.$vb.plugin.message.error("获取商品类别失败");
+          }
+        });
+    }
+  },
+  props: {
+    // 父组件传的数据
+    parentData: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    },
+    layerid: {
+      // 当前弹层id，用于关闭弹层
+      type: String,
+      default: ""
+    },
+    lydata: {
+      // 该值为父组件data的浅拷贝,更改父窗口传递的数据的时候，可以直接使用lydata来修改
+      type: Object,
+      default: () => {
+        return {};
+      }
+    },
+    content: {}
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "~@/assets/scss/commodity-category-edit-page";
+</style>
--- src/views/commodity-category-list-page.vue
+++ src/views/commodity-category-list-page.vue
@@ -1,13 +1,146 @@
 <!--商品类别-->
 <template>
-  <div>
-    商品类别
+  <div class="table-panel">
+    <div class="search-bar" @keydown.enter="search">
+      <el-input
+        v-model="searchForm.name"
+        placeholder="请输入昵称"
+        size="small"
+      />
+      <el-button
+        type="primary"
+        icon="el-icon-search"
+        size="small"
+        @click="search"
+        >查询</el-button
+      >
+      <el-button type="primary" plain size="small" @click="handleClick(0)"
+        >增加</el-button
+      >
+    </div>
+    <el-table
+      :data="tableData"
+      style="width: 100%"
+      @row-click="openDetails"
+      v-loading.fullscreen="isLoading"
+    >
+      <el-table-column label="分类图">
+        <template slot-scope="scope">
+          <el-avatar :src="baseUrl + scope.row.icon" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="sortNumber" label="排序号" />
+      <el-table-column prop="name" label="类别名" />
+      <el-table-column fixed="right" width="360" label="操作">
+        <template slot-scope="scope">
+          <el-button
+            @click.stop="handleClick(1, scope.row)"
+            type="primary"
+            size="mini"
+            >变动</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="searchForm.pageNum"
+      :page-sizes="[10, 20, 30, 40]"
+      :page-size="searchForm.pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+      class="page"
+    >
+    </el-pagination>
   </div>
 </template>
 
 <script>
+import lodash from "lodash";
+import base from "@/api/base";
+import commodityCategoryEditPage from "@/views/commodity-category-edit-page.vue";
 export default {
-  name: "commodity-category-list-page"
+  name: "commodity-category-list-page",
+  data() {
+    return {
+      tabActiveName: "all",
+      tableData: [],
+      searchForm: {
+        name: "",
+        pageNum: 1,
+        pageSize: 10
+      },
+      currentPage: 1,
+      isLoading: false,
+      total: 0,
+      baseUrl: ""
+    };
+  },
+  mounted() {
+    this.$api.commodityCategoryMangeAPI.getCommodityCategoryList().then(res => {
+      if (lodash.isEqual(res.code, "success")) {
+        this.total = res.total;
+        this.tableData = res.data;
+        this.baseUrl = base.defaultBaseUrl;
+      } else {
+        this.$vb.plugin.message.error(`获取商品类别列表失败,${res.code}`);
+      }
+    });
+  },
+  methods: {
+    search() {
+      this.isLoading = true;
+      this.$api.commodityCategoryMangeAPI
+        .getCommodityCategoryList(this.searchForm)
+        .then(res => {
+          if (lodash.isEqual(res.code, "success")) {
+            this.tableData = res.data;
+            this.total = res.total;
+            this.isLoading = false;
+          } else {
+            this.$vb.plugin.message.error(res.msg);
+          }
+        });
+    },
+    handleClick(status, val) {
+      switch (status) {
+        case 0:
+          // 增加
+          this.$vb.plugin.openLayer(
+            commodityCategoryEditPage,
+            this,
+            { state: "add" },
+            "新增商品类别",
+            900,
+            400
+          );
+          break;
+        case 1:
+          // 变动
+          this.$vb.plugin.openLayer(
+            commodityCategoryEditPage,
+            this,
+            { state: "update", classifyId: val.classifyId },
+            "变动商品类别",
+            900,
+            400
+          );
+          break;
+      }
+    },
+    handleSizeChange(size) {
+      this.searchForm.pageSize = size;
+      this.search();
+    },
+    handleCurrentChange(page) {
+      this.searchForm.pageNum = page;
+      this.search();
+    },
+    openDetails(row) {
+      this.handleClick(2, row);
+    }
+  }
 };
 </script>
 
