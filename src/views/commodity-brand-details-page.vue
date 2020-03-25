a5cbcaf6c60ffaae2ddf39f12c477eb6d2fad613
erakon <13431445070@sina.cn>
2020-03-25 17:37:25 +0800

    添加商品分类、类别、品牌的详情页,对订单管理更改根据角色类型显示特别操作

---
+++ src/views/commodity-brand-details-page.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="detail-box" :class="parentData.colNum">
+    <div>
+      <div>品牌名</div>
+      <div>{{ detailsInfo.brandName }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsInfo: {}
+    };
+  },
+  mounted() {
+    const self = this;
+    this.$api.commodityBrandMangeAPI
+      .getCommodityBrandInfo({ brandId: this.parentData.brandId })
+      .then(res => {
+        self.detailsInfo = res.data;
+      });
+  },
+  props: {
+    // 父组件传的数据
+    parentData: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    }
+  },
+  layerid: {
+    // 当前弹层id，用于关闭弹层
+    type: String,
+    default: ""
+  },
+  lydata: {
+    // 该值为父组件data的浅拷贝,更改父窗口传递的数据的时候，可以直接使用lydata来修改
+    type: Object,
+    default: () => {
+      return {};
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "~@/assets/scss/user-list-details-page.scss";
+</style>
---
+++ src/views/commodity-category-details-page.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="detail-box" :class="parentData.colNum">
+    <div>
+      <div>类别图</div>
+      <div>
+        <el-avatar v-if="detailsInfo.icon" :src="detailsInfo.icon" />
+      </div>
+    </div>
+    <div>
+      <div>分类名</div>
+      <div>{{ detailsInfo.classifyName }}</div>
+    </div>
+    <div>
+      <div>类别名</div>
+      <div>{{ detailsInfo.name }}</div>
+    </div>
+    <div>
+      <div>排序号</div>
+      <div>{{ detailsInfo.sortNumber }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import baseUrl from "@/api/base";
+export default {
+  data() {
+    return {
+      detailsInfo: {}
+    };
+  },
+  mounted() {
+    const self = this;
+    this.$api.commodityCategoryMangeAPI
+      .getCommodityCategoryInfo({ categoryId: this.parentData.categoryId })
+      .then(res => {
+        res.data.icon = baseUrl.defaultBaseUrl + res.data.icon;
+        self.detailsInfo = res.data;
+      });
+  },
+  props: {
+    // 父组件传的数据
+    parentData: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    }
+  },
+  layerid: {
+    // 当前弹层id，用于关闭弹层
+    type: String,
+    default: ""
+  },
+  lydata: {
+    // 该值为父组件data的浅拷贝,更改父窗口传递的数据的时候，可以直接使用lydata来修改
+    type: Object,
+    default: () => {
+      return {};
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "~@/assets/scss/user-list-details-page.scss";
+</style>
