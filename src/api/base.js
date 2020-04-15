35376b130aa7a24e4c37f6e7586465c774af0169
chenfeng <chen1115684621@163.com>
2020-04-15 11:42:28 +0800

    客服与客服聊天

--- src/api/base.js
+++ src/api/base.js
@@ -1,10 +1,10 @@
 /* 接口域名管理*/
 
 const base = {
-  //defaultBaseUrl: "http://172.16.0.110",
-  //defaultWsUrl: "ws://172.16.0.110",
-    defaultBaseUrl: "http://localhost",
-    defaultWsUrl: "ws://localhost",
+  defaultBaseUrl: "http://172.16.0.110",
+  defaultWsUrl: "ws://172.16.0.110",
+  //defaultBaseUrl: "http://localhost",
+  //defaultWsUrl: "ws://localhost",
   mock: "/mock"
 };
 
